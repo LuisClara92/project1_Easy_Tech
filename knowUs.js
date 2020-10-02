3639bb3072a9b6fc1addec14b336c7a0c3a9c1af
LuisClara92 <lc.sbg.92@gmail.com>
2020-10-02 13:01:51 +0100

    almost finish the conctat form. insert txt about us

--- knowUs.js
+++ knowUs.js
@@ -1,13 +1,23 @@
+
+let tablinks = document.getElementsByClassName("tablinks");
+
+tablinks[0].className = tablinks[0].className.replace(" ", "active");
 function openTab(evt, options) {
-    var i, tabcontent, tablinks;
+    var i, tabcontent;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
     }
-    tablinks = document.getElementsByClassName("tablinks");
+    
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
+    console.log(tablinks[i].className);
+
     }
     document.getElementById(options).style.display = "block";
     evt.currentTarget.className += " active";
   }
+
+  openTab(event, 'SignUp')
+  
+  
