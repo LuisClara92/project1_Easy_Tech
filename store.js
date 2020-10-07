af14d5b7c91c8a3a87e8d9494e7f32167bb82aa1
Leandro Baseggio <leandrobaseggio@gmail.com>
2020-10-07 11:55:41 +0100

    STORE: Reestructured the page. Still needs Media Querires and linking JavaScript.

--- store.js
+++ store.js
@@ -6,27 +6,66 @@ function openDialog() {
 
 
 
+/* ************************************** SHOPPING CART *************************************** */
+
+
+// const cartWindow
+
+// function openWindow() {
+//     cartWindow = window.open("about:blank", "Shopping Cart", "width=750, height=423");
+// }
+
+
+
+// document.getElementByClass("storeCards").addEventListener("click", openWindow);
+
+// function openWindow() {
+//     document.getElementByClass("storeCards").open("about:blank", "Shopping Cart", "width=750, height=423");
+// }
+
+
+
+// const cartWindow
+
+// function openWindow() {
+//     cartWindow = window.open("about:blank", "Shopping Cart", "width=750, height=423");
+// }
 
 
 
 
 
-const cartWindow
+$('.minus-btn').on('click', function (e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $this.closest('div').find('input');
+    var value = parseInt($input.val());
 
-function openWindow() {
-    cartWindow = window.open("about:blank", "Shopping Cart", "width=750, height=423");
-}
+    if (value > 1) {
+        value = value - 1;
+    } else {
+        value = 0;
+    }
 
+    $input.val(value);
 
+});
 
+$('.plus-btn').on('click', function (e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $this.closest('div').find('input');
+    var value = parseInt($input.val());
 
+    if (value < 100) {
+        value = value + 1;
+    } else {
+        value = 100;
+    }
 
+    $input.val(value);
+});
 
-
-
-
-document.getElementByClass("storeCards").addEventListener("click", openWindow);
-
-function openWindow() {
-    document.getElementByClass("storeCards").open("about:blank", "Shopping Cart", "width=750, height=423");
-}
+$('.like-btn').on('click', function () {
+    $(this).toggleClass('is-active');
+});
