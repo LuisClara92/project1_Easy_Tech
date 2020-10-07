d13416898c3c5eefb01ed73f3799d247d66db843
Leandro Baseggio <leandrobaseggio@gmail.com>
2020-10-07 08:57:11 +0100

    Reorganized all files togethter. Remove the folder Shopping Cart

--- store.js
+++ store.js
@@ -6,7 +6,19 @@ function openDialog() {
 
 
 
+const cartWindow
 
+function openWindow() {
+    cartWindow = window.open("about:blank", "Shopping Cart", "width=750, height=423");
+}
+
+
+
+document.getElementByClass("storeCards").addEventListener("click", openWindow);
+
+function openWindow() {
+    document.getElementByClass("storeCards").open("about:blank", "Shopping Cart", "width=750, height=423");
+}
 
 
 
@@ -25,8 +37,43 @@ function openWindow() {
 
 
 
-document.getElementByClass("storeCards").addEventListener("click", openWindow);
 
-function openWindow() {
-    document.getElementByClass("storeCards").open("about:blank", "Shopping Cart", "width=750, height=423");
-}
+
+//  ***************************************** SHOPPING CART *****************************************
+
+
+
+$('.minus-btn').on('click', function (e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $this.closest('div').find('input');
+    var value = parseInt($input.val());
+
+    if (value > 1) {
+        value = value - 1;
+    } else {
+        value = 0;
+    }
+
+    $input.val(value);
+
+});
+
+$('.plus-btn').on('click', function (e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $this.closest('div').find('input');
+    var value = parseInt($input.val());
+
+    if (value < 100) {
+        value = value + 1;
+    } else {
+        value = 100;
+    }
+
+    $input.val(value);
+});
+
+$('.like-btn').on('click', function () {
+    $(this).toggleClass('is-active');
+});
